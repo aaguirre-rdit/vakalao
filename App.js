44b445572bebe6c4889dbf09c0e95f7f479e5989
Anne A <aaguirre.rdit@gmail.com>
2020-08-21 17:13:18 +0200

    installed navigation

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Kaixo!</Text>
       <StatusBar style="auto" />
     </View>
   );
