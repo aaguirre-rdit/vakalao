da93e6ab5521d83997edce449cf70bf561ac57b3
Anne A <aaguirre.rdit@gmail.com>
2020-08-27 09:16:51 +0200

    made first load request work

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import MainNavigation from './navigations/mainNavigation';
 
-export default function App() {
-  return (
-    <MainNavigation/>
-  );
+import { AsyncStorage } from "react-native";
+import { AppLoading } from 'expo';
+
+
+const welcomeShown = async () => {
+  return await AsyncStorage.getItem("@vakalao:WELCOME_SHOWN");
+};
+
+export default class App extends React.Component{
+  state = {
+    reqIsReady : false,
+    initialRoute : undefined
+  };
+  initialize = async () => {
+    let  hasToken = await AsyncStorage.getItem("@vakalao:WELCOME_SHOWN");
+    const initialRoute = hasToken ? "Home" : "Welcome";
+    this.setState({
+      initialRoute
+    })
+
+  };
+  render()
+  {
+
+    console.log(welcomeShown());
+    if (!this.state.reqIsReady) {
+      return (
+        <AppLoading
+          startAsync={this.initialize}
+          onFinish={() => this.setState({ reqIsReady: true })}
+          onError={console.warn}
+        />
+      ); }
+    return (
+      <MainNavigation initialRoute={this.state.initialRoute}/>
+    );
+
+  }
 }
 
 
--- navigations/mainNavigation.js
+++ navigations/mainNavigation.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from '../scenes/home';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../scenes/home";
+import Welcome from "../scenes/welcome";
+import { AsyncStorage } from "react-native";
+
+
+const welcomeShown = async () => {
+  return await AsyncStorage.getItem("@vakalao:WELCOME_SHOWN");
+};
 
 const Stack = createStackNavigator();
 
-const mainNavigation = () => {
-    return (
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+const initialRoute = !welcomeShown() ? "Home" : "Welcome";
+
+const mainNavigation = (props) => {
+  console.log(props.initialRoute);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={props.initialRoute}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Welcome" component={Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default mainNavigation;
