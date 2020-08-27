92ae4a29556690fa585922f40d73d4a58047fa9e
Anne A <aaguirre.rdit@gmail.com>
2020-08-27 09:36:34 +0200

    added function to save initial token

--- scenes/welcome.js
+++ scenes/welcome.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from "react-native";
 
 
 import {StatusBar} from "expo-status-bar";
 
-
+const saveToken = async () => {
+  return await AsyncStorage.setItem("@vakalao:WELCOME_SHOWN");
+};
 export default function Welcome() {
+  //saveToken(); <---- UNCOMMENT  THIS LATER!
   return (
     <View style={styles.container}>
       <Text>Welcome!</Text>
