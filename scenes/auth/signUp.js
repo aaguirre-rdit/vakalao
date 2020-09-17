67c8710dbc526e94ce5b6bc79bb99f8238d14516
Anne A <aaguirre.rdit@gmail.com>
2020-09-17 11:55:31 +0200

    sign in page backbone complete

---
+++ scenes/auth/signUp.js
@@ -0,0 +1,45 @@
+import React , {useState, useEffect} from 'react';
+import { StyleSheet } from 'react-native';
+import { View, Form, Button, Text} from 'native-base';
+const  SignIn = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Sign In</Text>
+      <Button onPress={()=>props.navigation.navigate('Home')}>
+        <Text>Go home</Text>
+      </Button>
+      <Button onPress={()=>props.navigation.navigate('SignIn')}>
+        <Text>Go back</Text>
+      </Button>
+    </View>
+  )
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F50057',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height:'75%'
+  },
+  textContainer: {
+    height:'80%',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    padding:'auto'
+  },
+  welcomeText: {
+    color:'#fff',
+    fontSize:36,
+
+  },
+  buttonStyle: {
+    backgroundColor:'#FCE4EC',
+
+    marginHorizontal:20,
+    borderRadius:25
+  }
+});
+
+export default SignIn;
