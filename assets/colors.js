f966ea3b00368b8c0a9773919cd054e93ce9a1f7
Anne A <aaguirre.rdit@gmail.com>
2020-10-12 21:17:06 +0200

    added material primary and secondary color codes - need to edit existing code

---
+++ assets/colors.js
@@ -0,0 +1,17 @@
+const primaryColor = '#e91e63';
+const primaryLightColor = '#ff6090';
+const secondaryColor ='#b2ebf2';
+const secondaryLightColor = '#e5ffff';
+const secondaryDarkColor  ='#81b9bf';
+const black = '#000';
+const white = '#fff';
+
+module.exports = {
+  primaryColor,
+  primaryLightColor,
+  secondaryColor,
+  secondaryDarkColor,
+  secondaryLightColor,
+  black,
+  white
+};
